fec720fa696103803d57fa9473e95b9f4b79f3ab
Simiraider <simon.flomenboim@gmail.com>
2025-08-06 08:20:41 -0300

    porkemon-unity

---
+++ Porkemon-Unity/Proyecto-Porkemon/Assets/Scripts/CargarEscena.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CargarEscena : MonoBehaviour
+{
+    public void CargarMenu()
+    {
+        SceneManager.LoadScene("Interfaz de Menu");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
